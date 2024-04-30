c6906e54abca1b50a3902bc943f601b4cd1298f6
vishalns17 <vishalns17@gmail.com>
2024-04-30 18:06:45 +0530

    Adding files

---
+++ sentiment-analysis/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import joblib
+
+model=joblib.load('sentiment-model.pkl')
+
+sentiment_labels={'0':'Negative','1':'Positive'}
+
+st.title('Sentiment Analysis')
+
+user_input = st.text_area("Enter your text here")
+
+if st.button('Predict'):
+
+    predicted_sentiment = model.predict([user_input])[0]
+    print("Predicted Label:"+str(predicted_sentiment))
+    predicted_sentiment_label = sentiment_labels[str(predicted_sentiment)]
+
+    st.info(f"Predicted Sentiment:{predicted_sentiment_label}")
